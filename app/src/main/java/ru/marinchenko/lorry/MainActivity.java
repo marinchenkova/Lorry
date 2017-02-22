2024646f8b894b8eb0c33f5b70dedbec8c478b04
marinchenkova <marinchenkovalentin@gmail.com>
2017-02-22 16:46:33 +0300

    Главное окно приложения - внешний вид

--- app/src/main/java/ru/marinchenko/lorry/MainActivity.java
+++ app/src/main/java/ru/marinchenko/lorry/MainActivity.java
@@ -1,13 +1,62 @@
 package ru.marinchenko.lorry;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "ru.marinchenko.lorry.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user clicks the Send button */
+/*    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+*/
+    /**
+     * Вызывается при нажатии кнопки "Настройки"
+     * @param view кнопка
+     */
+    public void toSettings(View view){
+        //TODO toSettings()
+    }
+
+
+    /**
+     * Вызывается при нажатии кнопки "НАЙТИ СЕТЬ"
+     * @param view кнопка
+     */
+    public void findNets(View view){
+        //TODO findNets()
+    }
+
+    /**
+     * Вызывается при переключении флажка "Подключаться автоматически"
+     * @param view флажок
+     */
+    public void setAutoConnect(View view){
+        //TODO setAutoConnect()
+    }
+
+    /**
+     * Вывод информации о времени обновления списка сетей
+     * @param view текстовое поле
+     */
+    public void showUpdateInfo(View view){
+        //TODO showUpdateInfo()
+    }
+
+
 }
